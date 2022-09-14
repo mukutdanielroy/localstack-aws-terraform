cd599c09f12960c5eb5c7fe88ab5a0571efb0d6e
Mukut Daniel Roy <mdanielr007@gmail.com>
2022-09-14 22:14:16 +0600

    s3 deploy

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "access_key" {
+  type = string
+  description = "aws access key"
+}
+
+variable "secret_key" {
+  type = string
+  description = "aws secret key"
+}
+
+variable "region" {
+  type = string
+  description = "aws region"
+}
+
+variable "bucket_name" {
+  type = string
+  description = "s3 bucket name"
+}
